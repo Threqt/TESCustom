6f384049f9ad421be4c2156ffb1f08cba0302e3e
Threqt <45086473+Threqt@users.noreply.github.com>
2018-11-18 15:57:25 -0600

    activity

--- index.js
+++ index.js
@@ -11,7 +11,7 @@ bot.on(`guildMemberAdd`, member => {
 
 bot.on("ready", async() => {
   console.log(`${bot.user.username} is online!`);
-  bot.user.setActivity("unbiased TES papers", {type: "WATCHING"});
+  bot.user.setActivity("Akame's sick covers", {type: "LISTENING"});
 });
 
 bot.on("message", async message => {
